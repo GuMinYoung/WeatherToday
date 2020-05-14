2a21dce52b4536f214d27bec479f7f262b9c7d98
MinyoungKoo <rnalsdud1014@naver.com>
2020-05-14 15:25:56 +0900

    화면 구성 : 날씨 세부 정보 화면 구성
    - 화면 상단에는 이미지를, 화면 하단에는 텍스트를 배치한다.

---
+++ 03WeatherToday/03WeatherToday/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  03WeatherToday
+//
+//  Created by 구민영 on 2020/05/14.
+//  Copyright © 2020 구민영. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
